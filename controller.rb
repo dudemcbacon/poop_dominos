58924ef5e4a8c1ad89c267e3875ced2c2991d9f8
Brandon Burnett <gentoolicious@gmail.com>
2015-01-31 19:45:08 -0800

    More stuff.

--- controller.rb
+++ controller.rb
@@ -1,5 +1,53 @@
 require 'sinatra'
+require 'rmagick'
 
-get '/' do
-    "Hello World!"
+WIDTH = 264
+HEIGHT = 176
+
+WHITE = 0
+BLACK = 1
+
+get '/start' do
+  image = start_page
+  prepare_image(image)
+end
+
+def start_page
+  canvas = Magick::Image.new(264, 176){self.background_color = 'white'}
+  gc = Magick::Draw.new
+  gc.font_weight = Magick::BoldWeight
+
+  gc.annotate(canvas, 0, 0, 0, -10, "Chose Wisely") {
+    self.gravity = Magick::CenterGravity
+    self.font = "Helvetica-Narrow-Bold"
+    self.pointsize = 40
+  }
+
+  gc.annotate(canvas, 0, 0, 2, 2, "Poop") {
+    self.gravity = Magick::SouthWestGravity
+    self.pointsize = 26
+    self.font = "Helvetica-Bold"
+  }
+
+  gc.annotate(canvas, 0, 0, 2, 2, "Dominos") {
+    self.gravity = Magick::SouthEastGravity
+    self.pointsize = 26
+    self.font = "Helvetica-Bold"
+  }
+  return canvas
+end
+
+def prepare_image(image)
+  image.rotate!(180)
+  pixels = image.export_pixels(0, 0, image.columns, image.rows, 'I')
+  pixels = pixels.map{|x| x > 0 ? WHITE : BLACK}
+  return format_pixels(pixels)
+end
+
+def format_pixels(pixels)
+  line = ""
+  pixels.each_slice(WIDTH) do |row|
+    line << [row.join].pack("B*")
+  end
+  return line
 end
